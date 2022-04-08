881125973aee7a9ec9f97199f49058100fe7f1e0
xarowi <103097419+xarowi@users.noreply.github.com>
2022-04-08 15:10:25 +0000

    News endpoint

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ const fastify = require("fastify")({ logger: true });
 // Needed for handling application/x-www-form-urlencoded body
 fastify.register(require("fastify-formbody"));
 
+/**
+ * News endpoint
+ * 
+ * Needed for launcher. Response is plain text
+ * with titles of news
+ */
+fastify.get("/api/json/dlc/news", function (request, reply) {
+  reply.code(404);
+  reply.send(``);
+});
+
 // Listening at port from environment
 fastify.listen(process.env.PORT, "0.0.0.0", function (err) {
   if (err) {
